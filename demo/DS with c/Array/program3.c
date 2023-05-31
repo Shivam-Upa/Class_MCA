b91420b922e1e2bd0f99b45653d8c2bcd9ee7602
Shivam Upadhyay <70527646+Shivam-Upa@users.noreply.github.com>
2023-05-31 22:42:47 +0530

    Update program3.c

--- demo/DS with c/Array/program3.c
+++ demo/DS with c/Array/program3.c
@@ -4,7 +4,7 @@ int main()
     int arr[5], arr1[5], arr2[5];
     for (int i = 0; i < 5; i++)
     {
-        scanf("%d", &arr[i]);
+//         scanf("%d", &arr[i]);
     }
     for (int i = 0; i < 5; i++)
     {
