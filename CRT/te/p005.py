cf6f9793eeee8397390b15306affbef101ec91c1
Shivam Upadhyay <70527646+Shivam-Upa@users.noreply.github.com>
2023-06-01 23:31:52 +0530

    Delete p005.py

--- CRT/te/p005.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a Number"))
-
-for i in range(int(n**(1/2)), 0, -1):
-    if n % i == 0 and i % 2 != 0:
-        print(i)
-    p = n // i
-    if n % p == 0 and p % 2 != 0:
-        print(p)
